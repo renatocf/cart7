071f843ddef63f63f264d2d94668b42f8ef33a89
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2023-01-14 13:34:43 -0300

    Inicializa projeto Spring Boot com IntelliJ

---
+++ src/main/java/com/example/cart7/Cart7Application.java
@@ -0,0 +1,13 @@
+package com.example.cart7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cart7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cart7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cart7/Cart7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cart7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cart7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
